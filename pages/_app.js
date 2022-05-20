786ef5581c212f22b6867de7a322b53dfbcc8d08
bilalafzal01 <mafzal.bscs18seecs@seecs.edu.pk>
2022-05-21 02:23:26 +0500

    feat(product): price fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import React, { Fragment } from 'react'
-import Router from 'next/router'
 import { Toaster } from 'react-hot-toast'
 import { wrapper } from '../store'
 
@@ -8,16 +7,6 @@ import 'swiper/swiper.scss'
 import 'rc-slider/assets/index.css'
 import 'react-rater/lib/react-rater.css'
 import '../assets/css/styles.scss'
-
-import * as gtag from './../utils/gtag'
-
-const isProduction = process.env.NODE_ENV === 'production'
-
-// only events on production
-if (isProduction) {
-  // Notice how we track pageview when route is changed
-  Router.events.on('routeChangeComplete', (url) => gtag.pageview(url))
-}
 
 const MyApp = ({ Component, pageProps }) => (
   <Fragment>
--- pages/product/[pid].js
+++ pages/product/[pid].js
@@ -1,19 +1,22 @@
-import { useState } from 'react';
-import Footer from '../../components/footer';
-import Layout from '../../layouts/Main';
-import Breadcrumb from '../../components/breadcrumb';
-import ProductsFeatured from '../../components/products-featured';
-import Gallery from '../../components/product-single/gallery';
-import Content from '../../components/product-single/content';
-import Description from '../../components/product-single/description';
-import Reviews from '../../components/product-single/reviews';
-import { server } from '../../utils/server'; 
+import { useState } from 'react'
+import Footer from '../../components/footer'
+import Layout from '../../layouts/Main'
+import Breadcrumb from '../../components/breadcrumb'
+import ProductsFeatured from '../../components/products-featured'
+import Gallery from '../../components/product-single/gallery'
+import Content from '../../components/product-single/content'
+import Description from '../../components/product-single/description'
+import Reviews from '../../components/product-single/reviews'
+import { server } from '../../utils/server'
+import products from '../../utils/data/products'
 
 export async function getServerSideProps({ query }) {
+  const pid = query.pid
+  // const res = await fetch(`${server}/api/product/${pid}`);
 
-  const pid = query.pid;
-  const res = await fetch(`${server}/api/product/${pid}`);
-  const product = await res.json();
+  // const product = await res.json();
+
+  const product = products.find((product) => product.id === pid)
 
   return {
     props: {
@@ -23,23 +26,27 @@ export async function getServerSideProps({ query }) {
 }
 
 const Product = ({ product }) => {
-  const [showBlock, setShowBlock] = useState('description');
+  const [showBlock, setShowBlock] = useState('description')
 
   return (
     <Layout>
       <Breadcrumb currentPage={product.name} />
 
-      <section className="product-single">
-        <div className="container">
-          <div className="product-single__content">
+      <section className='product-single'>
+        <div className='container'>
+          <div className='product-single__content'>
             <Gallery images={product.images} />
             <Content product={product} />
           </div>
 
-          <div className="product-single__info">
-            <div className="product-single__info-btns">
-              <button type="button" onClick={() => setShowBlock('description')} className={`btn btn--rounded ${showBlock === 'description' ? 'btn--active' : ''}`}>Description</button>
-              <button type="button" onClick={() => setShowBlock('reviews')} className={`btn btn--rounded ${showBlock === 'reviews' ? 'btn--active' : ''}`}>Reviews (2)</button>
+          <div className='product-single__info'>
+            <div className='product-single__info-btns'>
+              <button type='button' onClick={() => setShowBlock('description')} className={`btn btn--rounded ${showBlock === 'description' ? 'btn--active' : ''}`}>
+                Description
+              </button>
+              <button type='button' onClick={() => setShowBlock('reviews')} className={`btn btn--rounded ${showBlock === 'reviews' ? 'btn--active' : ''}`}>
+                Reviews (2)
+              </button>
             </div>
 
             <Description product={product} show={showBlock === 'description'} />
@@ -48,12 +55,12 @@ const Product = ({ product }) => {
         </div>
       </section>
 
-      <div className="product-single-page">
+      <div className='product-single-page'>
         <ProductsFeatured />
       </div>
       <Footer />
     </Layout>
-  );
+  )
 }
 
 export default Product
